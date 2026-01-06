6a158caca2de6a44317cfbab502cd56f023c82d1
s41288 <s41288@bj.ac.th>
2026-01-06 13:24:02 +0700

    Add files via upload

---
+++ midterm_18-16/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1767680693,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/tiledbackground.png",
+		"images/360_f_606876358_nu2hcq3rp7xdpxg6pwnamggaun0fb5tw-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
