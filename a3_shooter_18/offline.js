99f9f8617c67c582b9d020e0fe6f2e714426d720
s41288 <s41288@bj.ac.th>
2026-01-28 10:21:03 +0700

    Add files via upload

---
+++ a3_shooter_18/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1769570443,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/background.png",
+		"images/sprite-sheet0.png",
+		"images/playerr-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/explotion-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
